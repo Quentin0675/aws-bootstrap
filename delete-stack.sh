3fffb5e4772d724b616bb1751279645800f08529
quentin <you@example.com>
2021-03-31 21:02:10 +0200

    infra as code and autodeployments

---
+++ delete-stack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STACK_NAME=awsbootstrap 
+REGION=us-east-1 
+CLI_PROFILE=awsbootstrap
+
+EC2_INSTANCE_TYPE=t2.micro 
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation delete-stack \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
